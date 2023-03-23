a1ca0b0cacaa3e20c6459ce8893fbc84fbc6a3e2
DidierAgamez <128607560+DidierAgamez@users.noreply.github.com>
2023-03-23 07:09:03 -0500

    Update Package.py

--- Package.py
+++ Package.py
@@ -1,35 +1,35 @@
-class Package:  
-    def init(self, id: int = 0, weight: float = 0.0, description: str = '', cost: float = 0.0, calculate: float = 0.0):  # New Object
-        self._id = id
-        self._weight = weight
-        self._description = description
-        self._cost = cost
+class Package(object):
+
+    def __init__(self, id=0, weight=1.0, description="Description", cost=1.0):
+        self._id = 0 if id == None else id
+        self._weight = 1 if weight == 0 else weight
+        self._description = 'description' if 'description' == None else description
+        self._cost = 1.0 if cost == 0.0 else cost
 
     def get_id(self):
         return self._id
 
-    def set_id(self):
+    def set_id(self, id):
         self._id = 0 if id == None else id
 
-    def get_weight(self):
+    def get_weight(self, weight):
         return self._weight
 
-    def set_weight(self):
-        self._weight = 1 
+    def set_weight(self, weight):
+        self._weight = 1 if weight == 0 else weight
 
-    def set_description(self):
-        self._description
+    def set_description(self, description):
+        self._description = 'description' if 'description' == None else description
 
     def get_description(self):
         return self._description
 
     def set_cost(self, cost):
-        self._cost = 1.0 
+        self._cost = 1.0 if cost == 0.0 else cost
 
     def get_cost(self):
         return self._cost
 
     def calculate(self):
-        print(f"COSTO TOTAL ENVIO{self._description}->$ ", self._cost * self._weight)
-
-
+        print(f"Cost of delivery {self._description} is $ ",
+              self._cost * self._weight)
